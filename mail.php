84601aa16fd49a4f05a36d4cdb88e4f6730ab4c3
George Brata <george.brata@groupvery.com>
2020-09-07 15:01:51 +0300

    Updated contact form details

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-    $to = 'demo@site.com';
+    $to = 'snimobiliare@gmail.com';
     $firstname = $_POST["fname"];
     $email= $_POST["email"];
     $text= $_POST["message"];
@@ -8,7 +8,7 @@
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= "De la: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
@@ -16,16 +16,16 @@
             <td>'.$firstname.'  '.$laststname.'</td>
         </tr>
         <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
+        <tr><td>Telefon: '.$phone.'</td></tr>
+        <tr><td>Mesaj: '.$text.'</td></tr>
         
     </table>';
 
     if (@mail($to, $email, $message, $headers))
     {
-        echo 'The message has been sent.';
+        echo 'Mesajul a fost trimis.';
     }else{
-        echo 'failed';
+        echo 'Eroare!';
     }
 
 ?>
